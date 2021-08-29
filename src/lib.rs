95ae854045ef0539941c3786d6b00827fcab571a
Per Kulseth Dahl <pkdahl@gmail.com>
2021-08-29 11:40:39 +0200

    Restructure (3.8.5.2.1 Making Space)

--- src/lib.rs
+++ src/lib.rs
@@ -1,20 +1,8 @@
-//! lib.rs
+//! src/lib.rs
+pub mod configuration;
+pub mod routes;
+pub mod startup;
 
-use actix_web::dev::Server;
-use actix_web::{web, App, HttpResponse, HttpServer};
-use std::net::TcpListener;
+// use actix_web::dev::Server;
+// use actix_web::{web, App, HttpResponse, HttpServer};
 
-pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
-    let server = HttpServer::new(|| {
-        App::new()
-            .route("/health_check", web::get().to(health_check))
-    })
-    .listen(listener)?
-    .run();
-
-    Ok(server)
-}
-
-async fn health_check() -> HttpResponse {
-    HttpResponse::Ok().finish()
-}
